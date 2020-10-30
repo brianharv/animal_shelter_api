b84f2be71369e27731d319a087bff357305d34a8
Brian Harvey <brian.harv3y@gmail.com>
2020-10-30 14:46:59 -0700

    write and pass POST exceptions handling test

--- spec/api/v1/requests/exception_handling_spec.rb
+++ spec/api/v1/requests/exception_handling_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
-describe "Bad PUT request error", :type => :request do
+describe "Exception Handling", :type => :request do
  
-  before { get '/api/v1/pets/12343' }
+  describe 'bad POST request for create method' do
+    before { post '/api/v1/pets' }
+    it 'should return a 400 Bad Request' do
+      expect(response).to have_http_status(:bad_request)
+    end
+  end  
 
-  it 'should return a 404 code' do
-  expect(response).to have_http_status(:not_found)    
-  end
+
+  describe 'bad GET request for show' do
+    before { get '/api/v1/pets/12343' }
+    it 'should return a 404 code for bad GET requst' do
+      expect(response).to have_http_status(:not_found)    
+    end
+  end  
 end
